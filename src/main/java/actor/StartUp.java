18f5a9b31db67dad2d5664ac016ecd707315a02d
James Covert <jcovert2@asu.edu>
2021-11-15 13:50:31 -0800

    StartUp attributes added

--- src/main/java/actor/StartUp.java
+++ src/main/java/actor/StartUp.java
@@ -2,9 +2,39 @@ package main.java.actor;
 
 public class StartUp extends Actor {
     
+    private double netIncome;
+    private double revenue;
+    private double marketShare;
+    
     public StartUp(String name, String description) {
         setName(name);
         setDescription(description);
+    }
+    
+    // getters and setters
+
+    public double getNetIncome() {
+        return netIncome;
+    }
+
+    public void setNetIncome(double netIncome) {
+        this.netIncome = netIncome;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(double revenue) {
+        this.revenue = revenue;
+    }
+
+    public double getMarketShare() {
+        return marketShare;
+    }
+
+    public void setMarketShare(double marketShare) {
+        this.marketShare = marketShare;
     }
 
 }
