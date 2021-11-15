ee5268109c2bc3c152015d314ca202a97ce0327b
James Covert <jcovert2@asu.edu>
2021-11-15 15:46:51 -0800

    added isEven() method to Quarter class

--- src/main/java/world/Quarter.java
+++ src/main/java/world/Quarter.java
@@ -7,9 +7,9 @@ public class Quarter {
     final static int Q3 = 2;
     final static int Q4 = 3;
     
-    private int currentQuarter;
-    
-    private int currentDay;
+    private int currentQuarter; 
+    private int currentDay; 
+    private boolean isEven;
     
     public Quarter(int currentQuarter, int currentDay) {
         
@@ -40,6 +40,8 @@ public class Quarter {
             
             this.currentQuarter = Q1;
         }
+        
+        setEven();
     }
 
     public void incrementQuarter() {
@@ -77,5 +79,21 @@ public class Quarter {
     public int getCurrentDay() {
         
         return this.currentDay;
+    }
+
+    public boolean isEven() {
+        return isEven;
+    }
+
+    private void setEven() {
+
+        if((this.currentQuarter % 2) == 0) {
+            
+            this.isEven = true;
+            
+        } else {
+            
+            this.isEven = false;
+        }
     }
 }
