f99f88800eb83a1dc8bae11c5e85bdbae9f9d857
James Covert <jcovert2@asu.edu>
2021-11-14 21:43:39 -0800

    gradle set up and basic test set up

--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -1,10 +1,19 @@
 package main.java.start;
 
-public class Main {
+public class Main extends Thread {
 
     public static void main(String[] args) {
 
+        Main t = new Main();
+        t.start();
+    }
+    
+    public void run() {
+        
         System.out.println("Wello Horld");
     }
+    
+    
+    
 
 }
